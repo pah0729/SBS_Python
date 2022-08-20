5eebcfbd965b0501b88b8a7406af0d87a9ad20a1
User <pah07292@gmail.com>
2022-08-20 14:53:03 +0900

    220820 수업 내용

--- 220820/조건문_예제.py
+++ 220820/조건문_예제.py
@@ -20,24 +20,24 @@ else:
 # 응용 예제 1
 # 점수를 입력받아서 학점을 출력하는 프로그램을 구현
 # 100~90 : A, 89~80 : B, 79~70 : C, 69~60 : D, 59~0 : F
+score = int(input('점수를 입력하세요 >>> '))
 
-credit = int(input(print('점수를 입력하세요 >>> ')))
-
-if credit >= 90:
-    print('A 입니다!')
-elif credit >= 80:
-    print('B 입니다!')
-elif credit >= 70:
-    print('C 입니다!')
-elif credit >= 60:
-    print('D 입니다!')
+if score >= 90:
+    credit = 'A'
+elif score >= 80:
+    credit = 'B'
+elif score >= 70:
+    credit = 'C'
+elif score >= 60:
+    credit = 'D'
 else:
-    print('F 입니다!')
+    credit = 'F'
+
+print(f'입력하신 점수는 {score}점이고, {credit}입니다!')
 
 # 응용 예제 2
 # 임의의 정수를 입력받은 뒤 3의 배수인지 아닌지 판별
-
-multiple_of_3 = int(input(print('정수를 입력하세요 >>> ')))
+multiple_of_3 = int(input('정수를 입력하세요 >>> '))
 
 if multiple_of_3%3 == 0:
     print(multiple_of_3, '은/는 3의 배수입니다.', sep='')
@@ -46,9 +46,9 @@ else:
 
 # 응용 예제 3
 # 임의의 정수 세개를 입력 받아 그 중 가장 큰 수를 출력
-essence_1st = int(input(print('첫번째 정수 입력')))
-essence_2nd = int(input(print('두번째 정수 입력')))
-essence_3rd = int(input(print('세번째 정수 입력')))
+essence_1st = int(input('첫번째 정수 입력'))
+essence_2nd = int(input('두번째 정수 입력'))
+essence_3rd = int(input('세번째 정수 입력'))
 
 if essence_1st > essence_2nd and essence_1st > essence_3rd:
     print('가장 큰 수는 %i' %essence_1st)
@@ -60,9 +60,35 @@ else:
 # 응용 예제 4
 # 미세먼지 저감 활동의 일환으로 차량 2부제 실시
 # 차량번호가 짝수 -> 운행가능, 홀수 -> 운행 불가
+"""
+숫자만 받을 경우
 car_num = list(map(int, input('차량번호를 입력하세요 ex) 1234 >>> ')))
-
 if car_num[-1]%2 == 0:
     print('오늘 운행가능입니다.')
 else:
     print('오늘은 운행불가입니다.')
+"""
+car_num = input('차량번호를 입력하세요 >>> ')
+last_car_num = int(car_num[-1])
+
+if last_car_num%2 == 0:
+    print('오늘 운행가능입니다.')
+else:
+    print('오늘은 운행불가입니다.')
+
+# 응용 예제 5
+# 1 ~ 100까지의 숫자중 홀수의 합, 짝수의 합
+synthesis = 0
+even = 0
+odd_num = 0
+
+for i in range(1, 101):
+    synthesis = synthesis + i
+    if i%2 == 0:
+        even = even + i
+    else:
+        odd_num = odd_num + i
+
+print(synthesis)
+print(even)
+print(odd_num)
